ee9891bd05044faefc96642126ae3dd0c736d778
Michael <MichaelWidler@yahoo.com>
2023-09-05 11:20:58 -0700

    Create constants for WSFonts.

---
+++ WSFontConstants.java
@@ -0,0 +1,255 @@
+
+package WidlerSuite;
+
+public interface WSFontConstants
+{
+	// row 0
+	public static final int BLANK_TILE = 0 + (0 * 16);
+	public static final int BOLT_TILE = 1 + (0 * 16);
+	public static final int CIRCLE_TILE = 2 + (0 * 16);
+	public static final int HEART_TILE = 3 + (0 * 16);
+	public static final int DIAMOND_TILE = 4 + (0 * 16);
+	public static final int CLUB_TILE = 5 + (0 * 16);
+	public static final int SPADE_TILE = 6 + (0 * 16);
+	public static final int DOT_TILE = 7 + (0 * 16);
+	public static final int INVERSE_DOT_TILE = 8 + (0 * 16);
+	public static final int RING_TILE = 9 + (0 * 16);
+	public static final int INVERSE_RING_TILE = 10 + (0 * 16);
+	public static final int MALE_TILE = 11 + (0 * 16);
+	public static final int FEMALE_TILE = 12 + (0 * 16);
+	public static final int SINGLE_NOTE_TILE = 13 + (0 * 16);
+	public static final int DOUBLE_NOTE_TILE = 14 + (0 * 16);
+	public static final int BURST_TILE = 15 + (0 * 16);
+
+	// row 1
+	public static final int RIGHT_TRIANGE_TILE = 0 + (1 * 16);
+	public static final int LEFT_TRIANGLE_TILE = 1 + (1 * 16);
+	public static final int UP_DOWN_ARROW_TILE = 2 + (1 * 16);
+	public static final int DOUBLE_EXCLAMATION_TILE = 3 + (1 * 16);
+	public static final int PARAGRAPH_TILE = 4 + (1 * 16);
+	public static final int SECTION_TILE = 5 + (1 * 16);
+	public static final int CURSOR_TILE = 6 + (1 * 16);
+	public static final int UP_DOWN_ARROW_WITH_BASE_TILE = 7 + (1 * 16);
+	public static final int UP_ARROW_TILE = 8 + (1 * 16);
+	public static final int DOWN_ARROW_TILE = 9 + (1 * 16);
+	public static final int RIGHT_ARROW_TILE = 10 + (1 * 16);
+	public static final int LEFT_ARROW_TILE = 11 + (1 * 16);
+	public static final int RIGHT_ANGLE_TILE = 12 + (1 * 16);
+	public static final int LEFT_RIGHT_ARROW_TILE = 13 + (1 * 16);
+	public static final int UP_TRIANGLE_TILE = 14 + (1 * 16);
+	public static final int DOWN_TRIANGLE_TILE = 15 + (1 * 16);
+
+	// row 2
+	public static final int SPACE_TILE = 0 + (2 * 16);
+	public static final int EXCLAMATION_TILE = 1 + (2 * 16);
+	public static final int DOUBLE_QUOTE_TILE = 2 + (2 * 16);
+	public static final int OCTOTHORPE_TILE = 3 + (2 * 16);
+	public static final int DOLLAR_TILE = 4 + (2 * 16);
+	public static final int PERCENT_TILE = 5 + (2 * 16);
+	public static final int AMPERSAND_TILE = 6 + (2 * 16);
+	public static final int APOSTROPHE_TILE = 7 + (2 * 16);
+	public static final int LEFT_PARENTHESIS_TILE = 8 + (2 * 16);
+	public static final int RIGHT_PARENTHESIS_TILE = 9 + (2 * 16);
+	public static final int ASTERIX_TILE = 10 + (2 * 16);
+	public static final int PLUS_TILE = 11 + (2 * 16);
+	public static final int COMMA_TILE = 12 + (2 * 16);
+	public static final int HYPHEN_TILE = 13 + (2 * 16);
+	public static final int PERIOD_TILE = 14 + (2 * 16);
+	public static final int SLASH_TILE = 15 + (2 * 16);
+
+	// row 3
+	public static final int ZER0_TILE = 0 + (3 * 16);
+	public static final int ONE_TILE = 1 + (3 * 16);
+	public static final int TWO_TILE = 2 + (3 * 16);
+	public static final int THREE_TILE = 3 + (3 * 16);
+	public static final int FOUR_TILE = 4 + (3 * 16);
+	public static final int FIVE_TILE = 5 + (3 * 16);
+	public static final int SIX_TILE = 6 + (3 * 16);
+	public static final int SEVEN_TILE = 7 + (3 * 16);
+	public static final int EIGHT_TILE = 8 + (3 * 16);
+	public static final int NINE_TILE = 9 + (3 * 16);
+	public static final int COLON_TILE = 10 + (3 * 16);
+	public static final int SEMICOLON_TILE = 11 + (3 * 16);
+	public static final int LESS_THAN_TILE = 12 + (3 * 16);
+	public static final int EQUALS_TILE = 13 + (3 * 16);
+	public static final int GREATER_THAN_TILE = 14 + (3 * 16);
+	public static final int QUESTION_TILE = 15 + (3 * 16);
+
+	// row 4
+	public static final int AT_TILE = 0 + (4 * 16);
+	public static final int CAPITAL_A_TILE = 1 + (4 * 16);
+	public static final int CAPITAL_B_TILE = 2 + (4 * 16);
+	public static final int CAPITAL_C_TILE = 3 + (4 * 16);
+	public static final int CAPITAL_D_TILE = 4 + (4 * 16);
+	public static final int CAPITAL_E_TILE = 5 + (4 * 16);
+	public static final int CAPITAL_F_TILE = 6 + (4 * 16);
+	public static final int CAPITAL_G_TILE = 7 + (4 * 16);
+	public static final int CAPITAL_H_TILE = 8 + (4 * 16);
+	public static final int CAPITAL_I_TILE = 9 + (4 * 16);
+	public static final int CAPITAL_J_TILE = 10 + (4 * 16);
+	public static final int CAPITAL_K_TILE = 11 + (4 * 16);
+	public static final int CAPITAL_L_TILE = 12 + (4 * 16);
+	public static final int CAPITAL_M_TILE = 13 + (4 * 16);
+	public static final int CAPITAL_N_TILE = 14 + (4 * 16);
+	public static final int CAPITAL_O_TILE = 15 + (4 * 16);
+
+	// row 5
+	public static final int CAPITAL_P_TILE_TILE = 0 + (5 * 16);
+	public static final int CAPITAL_Q_TILE_TILE = 1 + (5 * 16);
+	public static final int CAPITAL_R_TILE_TILE = 2 + (5 * 16);
+	public static final int CAPITAL_S_TILE_TILE = 3 + (5 * 16);
+	public static final int CAPITAL_T_TILE_TILE = 4 + (5 * 16);
+	public static final int CAPITAL_U_TILE_TILE = 5 + (5 * 16);
+	public static final int CAPITAL_V_TILE_TILE = 6 + (5 * 16);
+	public static final int CAPITAL_W_TILE_TILE = 7 + (5 * 16);
+	public static final int CAPITAL_X_TILE_TILE = 8 + (5 * 16);
+	public static final int CAPITAL_Y_TILE_TILE = 9 + (5 * 16);
+	public static final int CAPITAL_Z_TILE_TILE = 10 + (5 * 16);
+	public static final int LEFT_SQUARE_BRACKET_TILE = 11 + (5 * 16);
+	public static final int BACKSLASH_TILE = 12 + (5 * 16);
+	public static final int RIGHT_SQUARE_BRACKET_TILE = 13 + (5 * 16);
+	public static final int CARET_TILE = 14 + (5 * 16);
+	public static final int UNDERSCORE_TILE = 15 + (5 * 16);
+
+	// row 6
+	public static final int BACKTICK_TILE = 0 + (6 * 16);
+	public static final int SMALL_A_TILE = 1 + (6 * 16);
+	public static final int SMALL_B_TILE = 2 + (6 * 16);
+	public static final int SMALL_C_TILE = 3 + (6 * 16);
+	public static final int SMALL_D_TILE = 4 + (6 * 16);
+	public static final int SMALL_E_TILE = 5 + (6 * 16);
+	public static final int SMALL_F_TILE = 6 + (6 * 16);
+	public static final int SMALL_G_TILE = 7 + (6 * 16);
+	public static final int SMALL_H_TILE = 8 + (6 * 16);
+	public static final int SMALL_I_TILE = 9 + (6 * 16);
+	public static final int SMALL_J_TILE = 10 + (6 * 16);
+	public static final int SMALL_K_TILE = 11 + (6 * 16);
+	public static final int SMALL_L_TILE = 12 + (6 * 16);
+	public static final int SMALL_M_TILE = 13 + (6 * 16);
+	public static final int SMALL_N_TILE = 14 + (6 * 16);
+	public static final int SMALL_O_TILE = 15 + (6 * 16);
+
+	// row 7
+	public static final int SMALL_P_TILE = 0 + (7 * 16);
+	public static final int SMALL_Q_TILE = 1 + (7 * 16);
+	public static final int SMALL_R_TILE = 2 + (7 * 16);
+	public static final int SMALL_S_TILE = 3 + (7 * 16);
+	public static final int SMALL_T_TILE = 4 + (7 * 16);
+	public static final int SMALL_U_TILE = 5 + (7 * 16);
+	public static final int SMALL_V_TILE = 6 + (7 * 16);
+	public static final int SMALL_W_TILE = 7 + (7 * 16);
+	public static final int SMALL_X_TILE = 8 + (7 * 16);
+	public static final int SMALL_Y_TILE = 9 + (7 * 16);
+	public static final int SMALL_Z_TILE = 10 + (7 * 16);
+	public static final int LEFT_CURLY_BRACKET_TILE = 11 + (7 * 16);
+	public static final int PIPE_TILE = 12 + (7 * 16);
+	public static final int RIGHT_CURLY_BRACKET_TILE = 13 + (7 * 16);
+	public static final int TILDE_TILE = 14 + (7 * 16);
+	public static final int HOUSE_TILE = 15 + (7 * 16);
+
+	// row 8
+	public static final int DIE_ONE_TILE = 0 + (8 * 16);
+	public static final int SMALL_U_UMLAUT_TILE = 1 + (8 * 16);
+	public static final int SMALL_E_ACCUTE_TILE = 2 + (8 * 16);
+	public static final int SMALL_A_CIRCUMFLEX_TILE = 3 + (8 * 16);
+	public static final int SMALL_A_UMLAUT_TILE = 4 + (8 * 16);
+	public static final int SMALL_A_GRAVE_TILE = 5 + (8 * 16);
+	public static final int DIE_TWO_TILE = 6 + (8 * 16);
+	public static final int DIE_THREE_TILE = 7 + (8 * 16);
+	public static final int SMALL_E_CIRCUMFLEX_TILE = 8 + (8 * 16);
+	public static final int SMALL_E_UMLAUT_TILE = 9 + (8 * 16);
+	public static final int SMALL_E_GRAVE_TILE = 10 + (8 * 16);
+	public static final int SMALL_I_UMLAUT_TILE = 11 + (8 * 16);
+	public static final int SMALL_I_CIRCUMFLEX_TILE = 12 + (8 * 16);
+	public static final int SMALL_I_GRAVE_TILE = 13 + (8 * 16);
+	public static final int CAPITAL_A_UMLAUT_TILE = 14 + (8 * 16);
+	public static final int DIE_FOUR_TILE = 15 + (8 * 16);
+
+	// row 9
+	public static final int CAPITAL_E_ACCUTE_TILE = 0 + (9 * 16);
+	public static final int SMALL_AE_TILE = 1 + (9 * 16);
+	public static final int CAPITAL_AE_TILE = 2 + (9 * 16);
+	public static final int SMALL_O_CIRCUMFLEX_TILE = 3 + (9 * 16);
+	public static final int SMALL_O_UMLAUT_TILE = 4 + (9 * 16);
+	public static final int SMALL_O_GRAVE_TILE = 5 + (9 * 16);
+	public static final int SMALL_U_CIRCUMFLEX_TILE = 6 + (9 * 16);
+	public static final int SMALL_U_GRAVE_TILE = 7 + (9 * 16);
+	public static final int SMALL_Y_UMLAUT_TILE = 8 + (9 * 16);
+	public static final int CAPITAL_O_UMLAUT_TILE = 9 + (9 * 16);
+	public static final int CAPITAL_U_UMLAUT_TILE = 10 + (9 * 16);
+	public static final int CENT_TILE = 11 + (9 * 16);
+	public static final int POUND_TILE = 12 + (9 * 16);
+	public static final int YEN_TILE = 13 + (9 * 16);
+	public static final int PESETA_TILE = 14 + (9 * 16);
+	public static final int FUNCTION_TILE = 15 + (9 * 16);
+
+	// row 10
+	public static final int SMALL_A_ACCUTE_TILE = 0 + (10 * 16);
+	public static final int SMALL_I_ACCUTE_TILE = 1 + (10 * 16);
+	public static final int SMALL_O_ACCUTE_TILE = 2 + (10 * 16);
+	public static final int SMALL_U_ACCUTE_TILE = 3 + (10 * 16);
+	public static final int SMALL_N_WITH_TILDE_TILE = 4 + (10 * 16);
+	public static final int CAPITAL_N_WITH_TILDE_TILE = 5 + (10 * 16);
+	public static final int DIE_FIVE_TILE = 6 + (10 * 16);
+	public static final int DIE_SIX_TILE = 7 + (10 * 16);
+	public static final int INVERTED_QUESTION_TILE = 8 + (10 * 16);
+	public static final int REVERSED_NOT_TILE = 9 + (10 * 16);
+	public static final int NOT_TILE = 10 + (10 * 16);
+	public static final int ROTATE_CLOCKWISE_TILE = 11 + (10 * 16);
+	public static final int ROTATE_COUNTERCLOCKWISE_TILE = 12 + (10 * 16);
+	public static final int INVERTED_EXCLAMATION_TILE = 13 + (10 * 16);
+	public static final int LEFT_DOUBLE_ANGLE_TILE = 14 + (10 * 16);
+	public static final int RIGHT_DOUBLE_ANGLE_TILE = 15 + (10 * 16);
+
+	// row 11
+	public static final int LIGHT_SHADE_TILE = 0 + (11 * 16);
+	public static final int MEDIUM_SHADE_TILE = 1 + (11 * 16);
+	public static final int HEAVY_SHADE_TILE = 2 + (11 * 16);
+   
+   // Box-drawing tiles are not named
+
+	// row 13
+	public static final int FULL_BLOCK_TILE = 11 + (13 * 16);
+	public static final int HALF_BLOCK_BOTTOM_TILE = 12 + (13 * 16);
+	public static final int HALF_BLOCK_LEFT_TILE = 13 + (13 * 16);
+	public static final int HALF_BLOCK_RIGHT_TILE = 14 + (13 * 16);
+	public static final int HALF_BLOCK_TOP_TILE = 15 + (13 * 16);
+
+	// row 14
+	public static final int BLOCK_SEVEN_EIGHTHS_TILE = 0 + (14 * 16);
+	public static final int BLOCK_SIX_EIGHTHS_TILE = 1 + (14 * 16);
+	public static final int BLOCK_FIVE_EIGHTHS_TILE = 2 + (14 * 16);
+	public static final int BLOCK_FOUR_EIGHTHS_TILE = 3 + (14 * 16);
+	public static final int BLOCK_THREE_EIGHTHS_TILE = 4 + (14 * 16);
+	public static final int BLOCK_TWO_EIGHTHS_TILE = 5 + (14 * 16);
+	public static final int BLOCK_ONE_EIGHTH_TILE = 6 + (14 * 16);
+	public static final int SMALL_TAU_TILE = 7 + (14 * 16);
+	public static final int CAPITAL_PHI_TILE = 8 + (14 * 16);
+	public static final int CAPITAL_THETA_TILE = 9 + (14 * 16);
+	public static final int CAPITAL_OMEGA_TILE = 10 + (14 * 16);
+	public static final int SMALL_DELTA_TILE = 11 + (14 * 16);
+	public static final int INFINITY_TILE = 12 + (14 * 16);
+	public static final int SMALL_PHI_TILE = 13 + (14 * 16);
+	public static final int SMALL_EPSILON_TILE = 14 + (14 * 16);
+	public static final int INTERSECTION_TILE = 15 + (14 * 16);
+
+	// row 15
+	public static final int IDENTICAL_TO_TILE = 0 + (15 * 16);
+	public static final int PLUS_MINUS_TILE = 1 + (15 * 16);
+	public static final int GTE_TILE = 2 + (15 * 16);
+	public static final int LTE_TILE = 3 + (15 * 16);
+	public static final int INTEGRAL_UPPER_TILE = 4 + (15 * 16);
+	public static final int INTEGRAL_LOWER_TILE = 5 + (15 * 16);
+	public static final int DIVISION_TILE = 6 + (15 * 16);
+	public static final int ALMOST_EQUAL_TO_TILE = 7 + (15 * 16);
+	public static final int DEGREE_TILE = 8 + (15 * 16);
+	public static final int BULLET_TILE = 9 + (15 * 16);
+	public static final int SMALL_BULLET_TILE = 10 + (15 * 16);
+	public static final int SQUARE_ROOT_TILE = 11 + (15 * 16);
+	public static final int POWER_N_TILE = 12 + (15 * 16);
+	public static final int SQUARED_TILE = 13 + (15 * 16);
+	public static final int SQUARE_TILE = 14 + (15 * 16);
+	public static final int NOT_EQUAL_TILE = 15 + (15 * 16);
+
+}
+
